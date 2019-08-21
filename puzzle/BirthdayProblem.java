88950d72029e5cb79c64c712d7d2c5c2e8c47c8c
MrHeer <hlm52pk@163.com>
2019-08-21 15:22:57 +0800

    add Birthday problem

---
+++ puzzle/BirthdayProblem.java
@@ -0,0 +1,26 @@
+public class BirthdayProblem {
+    private static int findRepeat(int N) {
+        BitSet bitSet = new BitSet(N);
+        int next = StdRandom.uniform(N);
+        int count = 0;
+        while (!bitSet.get(next)) {
+            bitSet.set(next);
+            next = StdRandom.uniform(N);
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        int count;
+        int countTest = 0;
+        double avg = 0;
+        while (true) {
+            count = findRepeat(N);
+            countTest++;
+            avg = avg + (count - avg) / countTest;
+            StdOut.println(avg);
+        }
+    }
+}
